ad15e674526f5dfcafd3aee29bb2e3f74a96fbbc
YangxingZheng-NOAA <Yangxing.Zheng@noaa.gov>
2026-07-30 13:06:39 +0000

    add CICE_RESTART_POND_TOPO

--- ush/parsing_namelists_CICE.sh
+++ ush/parsing_namelists_CICE.sh
@@ -112,7 +112,8 @@ CICE_namelists() {
     local CICE_GRIDOCN="A" # A-grid for ocean (MOM6)
     local CICE_GRIDICE="B" # B-grid for seaice (CICE6)
     # tracer_nml section
-    local CICE_TR_POND_TOPO=${CICE_TR_POND_TOPO:-".false"}
+    local CICE_TR_POND_TOPO=${CICE_TR_POND_TOPO:-".false."}
+    local CICE_RESTART_POND_TOPO=${CICE_RESTART_POND_TOPO:-".false."}
     local CICE_TR_POND_LVL=${CICE_TR_POND_LVL:-".true."} # Use level melt ponds
     # (if CICE_TR_POND_LVL=true):
     #   -- if true, initialize the level ponds from restart (if runtype=continue)
